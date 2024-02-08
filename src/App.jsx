ff570cbe395d342cd6142ce246eae41e31dfcb0b
LaurentArcos <laurent.arcos@gmail.com>
2024-02-08 10:56:01 +0100

    affichage et tri ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
@@ -8,8 +8,21 @@ function App() {
   useEffect(() => {
     axios.get('https://seagale.fr/api/products?ws_key=24UCBP8LSPL5EWHZVI2VDSZMRSU7EITZ&filter[active]=[1]&display=full&output_format=JSON')
       .then(response => {
-        console.log(response.data); 
-        setProducts(response.data.products); 
+        console.log(response.data);
+
+        const sortedProducts = response.data.products.sort((a, b) => {
+          const refA = a.reference.split('.');
+          const refB = b.reference.split('.');
+
+          if (refA[0] < refB[0]) return -1;
+          if (refA[0] > refB[0]) return 1;
+
+          if (parseInt(refA[1], 10) < parseInt(refB[1], 10)) return -1;
+          if (parseInt(refA[1], 10) > parseInt(refB[1], 10)) return 1;
+
+          return parseInt(refA[2], 10) - parseInt(refB[2], 10);
+        });
+        setProducts(sortedProducts); 
       })
       .catch(error => console.error('Erreur lors de la récupération des données', error));
   }, []);
@@ -30,13 +43,11 @@ function App() {
         </thead>
         <tbody>
           {products.map((product, index) => {
-
             const [zone, famille, rang] = product.reference.split('.');
-
             return (
               <tr key={product.id}>
                 <td>{product.id}</td>
-                <td>{product.name[0].value}</td> 
+                <td>{product.name[0].value}</td>
                 <td>{index + 1}</td>
                 <td>{zone}</td>
                 <td>{famille}</td>
